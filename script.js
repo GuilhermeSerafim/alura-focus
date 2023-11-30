9d24cb486deefa0746e401b4a3e28f43ea95b02a
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-30 10:39:24 -0300

    Refatorando código, separando funções

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ const btLongo = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 
 btFoco.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco');
-    banner.setAttribute('src', '/imagens/foco.png');
+    alterarContexto('foco');
 })
 
 btCurto.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto');
-    banner.setAttribute('src', '/imagens/descanso-curto.png');
+    alterarContexto('descanso-curto');
 })
 
 btLongo.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo');
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo');
 })
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+}
