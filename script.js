a0d7abe3acb2eec8316b557ea85604f11aa9ffcd
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-30 09:59:11 -0300

    Manipulando o DOM com evento de click

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //Pegando elementos que vamos usar
 const html = document.querySelector('html');
-const btFoco = document.querySelector('.app_card_button--foco');
-const btCurto = document.querySelector('.app_card_button--curto');
-const btLongo = document.querySelector('.app_card_button--longo');
+const btFoco = document.querySelector('.app__card-button--foco');
+const btCurto = document.querySelector('.app__card-button--curto');
+const btLongo = document.querySelector('.app__card-button--longo');
+
+btFoco.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco');
+})
+
+btCurto.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto');
+})
+
+btLongo.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo');
+})
