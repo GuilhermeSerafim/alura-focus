2150b64f5a749a5fc239e73eb6df27dae2281fe2
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-30 10:22:41 -0300

    Mudando a imagem com evento de click, manipulando dom

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const html = document.querySelector('html');
 const btFoco = document.querySelector('.app__card-button--foco');
 const btCurto = document.querySelector('.app__card-button--curto');
 const btLongo = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image');
 
 btFoco.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco');
+    banner.setAttribute('src', '/imagens/foco.png');
 })
 
 btCurto.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png');
 })
 
 btLongo.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 })
