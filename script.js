95b65daa55f62b1c33196fea920fa6e8b1af57d3
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-30 11:10:20 -0300

    Removendo classe active com forEach em array de botões

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const btCurto = document.querySelector('.app__card-button--curto');
 const btLongo = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button'); //Array de botoes
 
 btFoco.addEventListener('click', () => {
-    alterarContexto('foco');
-    btFoco.classList.add('active');
+    alterarContexto('foco'); //Como a função vem antes de adicionar, ela percorre o array de botoes e limpa os estilos
+    btFoco.classList.add('active'); //Logo em seguida, adiciona se caso ele for clicado (a ultima etapa)
+    //Até outro ser clicado e esse ser removido no foreach do alterarContexto
 })
 
 btCurto.addEventListener('click', () => {
@@ -23,6 +25,10 @@ btLongo.addEventListener('click', () => {
 
 //Identificamos código repetido, logo criamos uma função para deixar o código mais inxtuo
 function alterarContexto(contexto) {
+    //Criado para limpar os estilos dos botões anteriores
+    botoes.forEach((contexto) => {
+        contexto.classList.remove('active');
+    })
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
     //O Inner html também é usado para fazer lista com +=
