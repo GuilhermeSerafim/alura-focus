c9c3f9c439775ec3111da04084be58c4943fca1e
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-30 10:53:05 -0300

    Manipulando textos com template string e innerHTML

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btFoco = document.querySelector('.app__card-button--foco');
 const btCurto = document.querySelector('.app__card-button--curto');
 const btLongo = document.querySelector('.app__card-button--longo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 btFoco.addEventListener('click', () => {
     alterarContexto('foco');
@@ -19,5 +20,22 @@ btLongo.addEventListener('click', () => {
 
 function alterarContexto(contexto) {
     html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    //O Inner html também é usado para fazer lista com +=
+    switch (contexto) {
+        case 'foco':
+            titulo.innerHTML = `Que tal dar uma respirada? <br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case 'descanso-curto':
+            titulo.innerHTML = `Que tal da uma respirada? <br>
+            <strong class="app__title-strong">Faça uma pausa curta.</strong>`
+            break;
+        case 'descanso-longo':
+            titulo.innerHTML = `Hora de voltar à superfície. <br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+    
+        default:
+            break;
+    }
 }
