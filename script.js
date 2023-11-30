e12c7363e6afbaa31f488ea3adad507dd57f0e0c
GuilhermeSerafim <guilerstudies@gmail.com>
2023-11-30 09:46:59 -0300

    Criando script com variaveis resgatadas do html, e aplicando defer no script para recarregar o html e por último o script

---
+++ script.js
@@ -0,0 +1,5 @@
+//Pegando elementos que vamos usar
+const html = document.querySelector('html');
+const btFoco = document.querySelector('.app_card_button--foco');
+const btCurto = document.querySelector('.app_card_button--curto');
+const btLongo = document.querySelector('.app_card_button--longo');
