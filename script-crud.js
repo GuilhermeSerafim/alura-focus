be59ae19d9ac716a97d8007ba917a626694ae416
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-06 10:15:05 -0300

    Pegando objeto tarefa que pegara o que for digitado no textarea, e assim que o form for enviado, enviara para o array tarefas

--- script-crud.js
+++ script-crud.js
@@ -1,6 +1,19 @@
 const btAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const textArea = document.querySelector('.app__form-textarea');
+const tarefas = []
+
 btAdicionarTarefa.addEventListener('click', () => {
     //toggle faz a alternância de classe
     formAdicionarTarefa.classList.toggle('hidden');
+});
+
+formAdicionarTarefa.addEventListener('submit', (event) => {
+    event.preventDefault();
+    //Criando objeto que representa a tarefa
+    const tarefa = {
+        descricao: textArea.value //Pegando valor digitado e armazenando em uma chave
+    }
+    tarefas.push(tarefa);
+    console.log(tarefas)
 });
