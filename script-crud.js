7ebb9421539de020a2d9f22b23b461475a72a2cd
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-06 10:26:53 -0300

    Armazenando nossa lista tarefas no armazenamento local

--- script-crud.js
+++ script-crud.js
@@ -15,5 +15,9 @@ formAdicionarTarefa.addEventListener('submit', (event) => {
         descricao: textArea.value //Pegando valor digitado e armazenando em uma chave
     }
     tarefas.push(tarefa);
-    console.log(tarefas)
+    //O que o js fez por debaixo dos panos, é pegar o array de objetos tarefas, e colocar toString
+    localStorage.setItem('tarefas', tarefas);
+    //Resultado: tarefas: object Object
+    //Para resolver isso, vamos usar uma API
+    //Em outras palavras, a API vai pegar um objeto e transformá-lo numa string de uma forma que consiga fazer o inverso depois - pegar a string e transformá-la num objeto.
 });
